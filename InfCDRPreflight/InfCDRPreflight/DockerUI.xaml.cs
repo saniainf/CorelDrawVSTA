dcf90bc3a4795d0b6d9d7c62131ab18b68dc1444
inf <saniainf@gmail.com>
2017-04-11 09:41:57 +0700

    preflight

---
+++ InfCDRPreflight/InfCDRPreflight/DockerUI.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using corel = Corel.Interop.VGCore;
+
+namespace InfCDRPreflight
+{
+
+    public partial class DockerUI : UserControl
+    {
+        private corel.Application corelApp;
+        public DockerUI(corel.Application app)
+        {
+            this.corelApp = app;
+            InitializeComponent();
+        }
+    }
+}
