9c767cdde1cf3e11100389235eb4babdae9c9b7a
inf <saniainf@gmail.com>
2017-04-12 14:16:51 +0700

    46465

--- InfCDRPreflight/InfCDRPreflight/DockerUI.xaml.cs
+++ InfCDRPreflight/InfCDRPreflight/DockerUI.xaml.cs
@@ -20,10 +20,78 @@ namespace InfCDRPreflight
     public partial class DockerUI : UserControl
     {
         private corel.Application corelApp;
+        private delegate void delAction(corel.Shape s);
+        static private Random rnd = new Random();
+
         public DockerUI(corel.Application app)
         {
             this.corelApp = app;
             InitializeComponent();
+        }
+
+        private void startAction(delAction action)
+        {
+            if (corelApp.Documents.Count == 0)
+                return;
+            corelApp.ActiveDocument.Unit = corel.cdrUnit.cdrMillimeter;
+
+            if (chxOnAllPage.IsChecked ?? false)
+                foreach (corel.Page page in corelApp.ActiveDocument.Pages)
+                    forEachShapeOnShapeRange(action, page.Shapes.All());
+            else
+                forEachShapeOnShapeRange(action, corelApp.ActivePage.Shapes.All());
+        }
+
+        private void forEachShapeOnShapeRange(delAction action, corel.ShapeRange sr)
+        {
+            corel.Shape s;
+            while (sr.Count > 0)
+            {
+                s = sr[1];
+                sr.Remove(1);
+                if (s.Type == Corel.Interop.VGCore.cdrShapeType.cdrGroupShape)
+                    forEachShapeOnShapeRange(action, s.Shapes.All());
+                if (s.PowerClip != null)
+                    forEachShapeOnShapeRange(action, s.PowerClip.Shapes.All());
+                action(s);
+            }
+        }
+
+        // convert methods
+
+        private void textToCurves(corel.Shape s)
+        {
+            if (s.Type == Corel.Interop.VGCore.cdrShapeType.cdrTextShape)
+                s.ConvertToCurves();
+        }
+
+        private void OLEshapesToCurves(corel.Shape s)
+        {
+            corel.Rect rect;
+            if (s.Type == Corel.Interop.VGCore.cdrShapeType.cdrOLEObjectShape)
+            {
+                rect = s.BoundingBox;
+                s.Cut();
+                corelApp.ActiveLayer.PasteSpecial("Metafile");
+                s = corelApp.ActiveSelection;
+                s.SetPosition(rect.Left, rect.Top);
+                s.SetSize(rect.Width, rect.Height);
+                forEachShapeOnShapeRange(textToCurves, s.Shapes.All());
+            }
+        }
+
+        // events
+
+        private void btnTextToCurves_Click(object sender, RoutedEventArgs e)
+        {
+            delAction action = textToCurves;
+            startAction(action);
+        }
+
+        private void btnOLEtoCurves_Click(object sender, RoutedEventArgs e)
+        {
+            delAction action = OLEshapesToCurves;
+            startAction(action);
         }
     }
 }
